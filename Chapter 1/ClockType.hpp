cfb703998a6e725375e5273e3aabb504e6119a2a
ograndoptimist <gabrieliprj@gmail.com>
2019-04-28 18:23:45 -0300

    altera 1

--- Chapter 1/ClockType.hpp
+++ Chapter 1/ClockType.hpp
@@ -1,14 +1,77 @@
 class ClockType {
 	public:
-		void setTime(int h, int m, int s);
-		Clockype getTime(void);
+		ClockType(int, int, int);
+		ClockType();
+		void setTime(int, int, int );
+		Clockype getTime(void) const;
 		void incrementHour(void);
 		void incrementMin(void);
 		void incrementSec(void);
-		void printTime(void);
-		void compareTime(ClockType time1, ClockType time2);		
+		void printTime(void) const;
+		bool compareTime(ClockType, ClockType) const;		
 	private:
 		int hour;
 		int min;
 		int sec;
 };
+
+
+ClockType::ClockType(int h, int m, int s)
+{
+	if(h < 24 && h >= 0) 
+	{
+		hour = h;
+	} else {
+		hour = 0;
+	}
+
+	if(m < 60 && m >= 0)
+	{
+		min = m;
+	} else {
+		min = 0;
+	}
+
+	if(s < 60 && s >= 0)
+	{
+		sec = s;
+	} else {
+		sec = 0;
+	}
+}
+
+ClockType::ClockType()
+{
+	hour = 0;
+	min = 0;
+	sec = 0;
+}
+
+void ClockType::setTime(int h, int m, int s)
+{
+	if(h < 24 && h >= 0) 
+	{
+		hour = h;
+	} else {
+		hour = 0;
+	}
+
+	if(m < 60 && m >= 0)
+	{
+		min = m;
+	} else {
+		min = 0;
+	}
+
+	if(s < 60 && s >= 0)
+	{
+		sec = s;
+	} else {
+		sec = 0;
+	}	
+}
+
+void ClockType::getTime()
+{
+	
+}
