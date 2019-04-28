abb2cec05724f46863291e72b2b32f8d0d36f925
ograndoptimist <gabrieliprj@gmail.com>
2019-04-28 19:30:19 -0300

    Linked listd

---
+++ Linked Links/SLLink.hpp
@@ -0,0 +1,26 @@
+class LinkedLink {
+	public:
+		LinkedLink();
+		int add(int);
+		int remove(void);		
+	private:
+		int data;
+		LinkedLink* head;
+		LinkedLink* tail;
+		LinkedLink* next;
+};
+
+LinkedLink::LinkedLink()
+{
+	next = NULL;
+	head = NULL;
+	tail = NULL;
+}
+
+int LinkedLink::add(int item) 
+{
+	if(head = NULL) 
+	{
+		
+	}	
+}
